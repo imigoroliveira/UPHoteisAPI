154afb4536e906ea67a4288aac5d769a1cb5f82d
imigoroliveira <igormat4us@gmail.com>
2023-11-20 13:02:09 -0300

    Pushing new changes

--- Program.cs
+++ Program.cs
@@ -38,7 +38,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-app.UseCors(opcoes => opcoes.AllowAnyOrigin().AllowAnyHeader());
+app.UseCors(opcoes => opcoes.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 app.UseAuthorization();
 
 app.MapControllers();
--- data/HotelAPIDbContext.cs
+++ data/HotelAPIDbContext.cs
@@ -1,17 +1,20 @@
+// HotelAPIDbContext.cs
+
 using UPHoteisAPI.Models;
 using Microsoft.EntityFrameworkCore;
 
-    namespace UPHoteisAPI.Data;
-
+namespace UPHoteisAPI.Data
+{
     public class HotelAPIDbContext : DbContext
     {
         public HotelAPIDbContext(DbContextOptions<HotelAPIDbContext> options) : base(options) { }
 
-        public DbSet<Reserva> Reservas { get; set;}
+        public DbSet<Reserva> Reservas { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Quarto> Quartos { get; set; }
         public DbSet<Hotel> Hoteis { get; set; }
         public DbSet<Avaliacao> Avaliacoes { get; set; }
         public DbSet<ServicoQuarto> Servicos { get; set; }
-        
+        public DbSet<Funcionario> Funcionarios { get; set; }
     }
+}
